51b6c83ddb650f7c7a36b4eb1b959ebdfee6c248
Brett Reinhard <brett@reinhards.us>
2017-03-11 14:55:47 -0800

    Update MainController.js

--- Angular/ExerciseLog/js/controllers/MainController.js
+++ Angular/ExerciseLog/js/controllers/MainController.js
@@ -1,4 +1,7 @@
+// Create MainController
+
 app.controller('MainController', ['$scope', function($scope) {
+  // Create Group of Exercises with a default count
   $scope.exercises = [
     {
       name: 'Pushups',
@@ -29,10 +32,14 @@ app.controller('MainController', ['$scope', function($scope) {
       count: 15
     }
   ];
+  
+  // Create function to increment exercise count
   $scope.increase = function(index){
     $scope.exercises[index].count = $scope.exercises[index].count + 1; 
     
   };
+    // Create function to decrement exercise count
+
   $scope.decrease = function(index){
     $scope.exercises[index].count = $scope.exercises[index].count - 1; 
     
